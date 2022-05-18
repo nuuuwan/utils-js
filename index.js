3b9030b993583f5ecbc60a6374231f389e2f494d
nuuuwan <nuuuwan@gmail.com>
2022-05-18 13:23:27 +0530

    * Imported DataStructures

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 import Cache from './src/Cache.js';
 import Color from './src/Color.js';
+import DataStructures from './src/DataStructures.js';
 import MathX from './src/MathX.js';
 import Random from './src/Random.js';
 import StringX from './src/StringX.js';
 import TimeX from './src/TimeX.js';
 import WWW from './src/WWW.js';
 
-export {Cache, Color, MathX, Random, StringX, TimeX, WWW};
+export {Cache, Color, DataStructures, MathX, Random, StringX, TimeX, WWW};
