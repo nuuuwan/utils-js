9c6ce64c39c115311a2ae0c55dc2ef4d446ce75d
nuuuwan <nuuuwan@gmail.com>
2022-05-18 11:08:12 +0530

    Added round, randomInt, randomChoice to MathX

--- src/MathX.js
+++ src/MathX.js
@@ -4,4 +4,23 @@ export default class MathX {
       return _sum + num;
     }, 0);
   }
+
+  static round(x, a) {
+    if (a < 1) {
+      const ra = parseInt(1 / a);
+      return parseInt(x * ra + 0.5) / ra;
+    } else {
+      return parseInt(x / a + 0.5) * a;
+    }
+  }
+
+  static randomInt(min, max) {
+    return parseInt(Math.random() * (max - min)) + min;
+  }
+
+  static randomChoice(arr) {
+    const n = arr.length;
+    const randomI = MathXFuture.randomInt(0, n);
+    return arr[randomI];
+  }
 }
